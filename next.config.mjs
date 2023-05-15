bcb24e8826ec762776701cb1ab65891be9f6e3ff
Liam Hales <liam.hales96@gmail.com>
2023-05-15 22:15:07 +0100

    Added eslint, next, tailwind and postcss config files

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * The Next.js config used to configure
+ * the Next.js framework for the app
+ *
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+};
+
+export default nextConfig;
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * The Tailwind CSS config used to
+ * configure Tailwind CSS for the app
+ *
+ * @type {import('tailwindcss').Config}
+ */
+const tailwindConfig = {
+  content: [
+    './src/**/*.{ts,tsx}',
+  ],
+};
+
+export default tailwindConfig;
