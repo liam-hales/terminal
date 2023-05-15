31815f45ea920b744cd64c6a1ab37c3d28112778
Liam Hales <liam.hales96@gmail.com>
2023-05-15 22:31:19 +0100

    Updated next config to use @next/bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
+import bundleAnalyser from '@next/bundle-analyzer';
+
+/**
+ * The initialised bundle analyser
+ * from `@next/bundle-analyzer`
+ */
+const withAnalyser = bundleAnalyser({
+  enabled: process.env.ANALYSE_BUNDLE === 'true',
+  openAnalyzer: true,
+});
+
 /**
  * The Next.js config used to configure
  * the Next.js framework for the app
@@ -9,4 +20,4 @@ const nextConfig = {
   productionBrowserSourceMaps: true,
 };
 
-export default nextConfig;
+export default withAnalyser(nextConfig);
