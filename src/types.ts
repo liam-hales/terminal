2c38ad10a14874befa911344c767c88da316e902
Liam Hales <liam.hales96@gmail.com>
2023-05-29 22:39:05 +0100

    Added feature interfaces and utility types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
-import { ReactElement, Ref } from 'react';
+import { FunctionComponent, ReactElement, Ref } from 'react';
+import { Schema, z } from 'zod';
+
+/**
+ * The feature ID used to differentiate
+ * each individual feature
+ */
+export type FeatureId = '';
 
 /**
  * The props that all component
@@ -25,6 +32,98 @@ export interface AsyncComponent<P extends Partial<Record<keyof P, unknown>> = ne
 }
 
 /**
+ * Used to describe a feature that can
+ * be used within the terminal
+ *
+ * - Generic type `F` for the feature ID
+ * - Generic type `S` for the command schema
+ * - Generic type `P` for the component props
+ */
+export interface IFeature<
+  F extends FeatureId,
+  S extends Schema,
+  P extends Record<keyof P, unknown>,
+> {
+  readonly id: F;
+  readonly command: Command<S, P>;
+  readonly component: FunctionComponent<P>;
+  readonly enabled: boolean;
+}
+
+/**
+ * Used to describe a feature command which
+ * can be executed in the terminal
+ *
+ * - Generic type `S` for the schema
+ * - Generic type `P` for the component props
+ */
+export interface Command<
+  S extends Schema,
+  P extends Record<keyof P, unknown>,
+> {
+  readonly name: string;
+  readonly options: CommandOptions<S>;
+  readonly action: (options: z.infer<S>) => P;
+}
+
+/**
+ * Used to describe the command options which consists of the `schema`
+ * used for validation and a `config` for each option
+ */
+export interface CommandOptions<T extends Schema> {
+  readonly schema: T;
+  readonly config: Record<keyof z.infer<T>, CommandOptionConfig>;
+}
+
+/**
+ * Used to describe the config
+ * for a command option
+ */
+export interface CommandOptionConfig {
+  readonly alias: string;
+  readonly description: string;
+}
+
+/**
+ * The utility type used to extract the feature `component`
+ * props type for a specific feature
+ *
+ * - Generic type `T` for the feature ID
+ */
+export type ExtractProps<T extends FeatureId> =
+  ReturnType<
+    ExtractCommand<T>['action']
+  >;
+
+/**
+ * The utility type used to extract the feature
+ * `command` type for a specific feature
+ *
+ * - Generic type `T` for the feature ID
+ */
+export type ExtractCommand<T extends FeatureId> =
+  Extract<
+    Feature,
+    {
+      readonly id: T;
+    }
+  >['command'];
+
+/**
+ * Used to build the feature output
+ * type for all features
+ *
+ * - Generic type `T` for the feature union type
+ */
+export type FeatureOutput<T extends Feature> =
+  T extends Feature
+    ? {
+        readonly featureId: T['id'];
+        readonly props: ExtractProps<T['id']>;
+      }
+    : never;
+
+/**
  * Describes the parsed input which consists of
  * the `command` and the `options`
  */
@@ -48,7 +147,7 @@ export interface TerminalInputBlock {
  */
 export interface TerminalOutputBlock {
   readonly type: 'output';
-  readonly value: unknown;
+  readonly value: FeatureOutput<Feature>;
 }
 
 /**
