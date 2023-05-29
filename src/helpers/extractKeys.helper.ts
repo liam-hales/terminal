056d8a75856f8c49867400f9c16a5eb940308e32
Liam Hales <liam.hales96@gmail.com>
2023-05-29 22:52:22 +0100

    Fixed extractKeys generic type and updated comment

--- src/helpers/extractKeys.helper.ts
+++ src/helpers/extractKeys.helper.ts
@@ -2,10 +2,12 @@
  * Used to extract the keys from a given object which is also typesafe
  * returning keys with a type of `(keyof T)` instead of `string[]`
  *
+ * - Generic type `T` for the object
+ *
  * @param from The object to extract keys from
  * @returns The objects keys
  */
-const extractKeys = <T extends Record<keyof T, unknown>>(from: T): (keyof T)[] => {
+const extractKeys = <T extends Partial<Record<keyof T, unknown>>>(from: T): (keyof T)[] => {
   return Object.keys(from) as (keyof T)[];
 };
 
