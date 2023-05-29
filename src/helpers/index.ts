f4abbe71576a7d88084ac19a2d8f6bbbedac7c4a
Liam Hales <liam.hales96@gmail.com>
2023-05-29 22:12:47 +0100

    Fixed helpers import order issue

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,9 @@
+import buildFeature from './buildFeature.helper';
 import extractKeys from './extractKeys.helper';
 import parseInput from './parseInput.helper';
-import buildFeature from './buildFeature.helper';
 
 export {
+  buildFeature,
   extractKeys,
   parseInput,
-  buildFeature,
 };
