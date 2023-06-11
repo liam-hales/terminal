5803c2ab41782b0de0c421f5ac8d18c2f26860cd
Liam Hales <liam.hales96@gmail.com>
2023-06-11 23:35:40 +0100

    Added TerminalProvider logic to execute an input and add blocks to the blocks state

--- src/providers/terminal.provider.tsx
+++ src/providers/terminal.provider.tsx
@@ -3,6 +3,7 @@
 import { FunctionComponent, ReactElement, ReactNode, useState } from 'react';
 import { TerminalContext } from '../context';
 import { BaseProps, TerminalBlock } from '../types';
+import { parseInput, executeInput } from '../helpers';
 
 /**
  * The `TerminalProvider` component props
@@ -28,11 +29,60 @@ interface Props extends BaseProps {
 const TerminalProvider: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
   const [blocks, setBlocks] = useState<TerminalBlock[]>([]);
 
+  /**
+   * Used to add a terminal block to
+   * the `blocks` state
+   *
+   * @param block The block to add
+   */
+  const addBlock = (block: TerminalBlock): void => {
+    setBlocks((current) => {
+      return [
+        block,
+        ...current,
+      ];
+    });
+  };
+
+  /**
+   * Used to execute a terminal command
+   * from a user `input`
+   *
+   * @param input The user input
+   */
+  const execute = (input: string): void => {
+
+    try {
+      const parsed = parseInput(input);
+      const output = executeInput(parsed);
+
+      // Add the terminal output block
+      // for the feature output
+      addBlock({
+        type: 'output',
+        input: input,
+        value: output,
+      });
+    }
+    catch (error) {
+      if (error instanceof Error) {
+
+        // Add the terminal error block
+        // for the caught error message
+        addBlock({
+          type: 'error',
+          input: input,
+          value: error.message,
+        });
+      }
+    }
+  };
+
   return (
     <TerminalContext.Provider value={
       {
         blocks: blocks,
-        execute: () => {},
+        execute: execute,
       }
     }
     >
