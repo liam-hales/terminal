fa0d8e85d5bc7a3c7f0702802e940d40c9206e96
Liam Hales <liam.hales96@gmail.com>
2023-06-16 18:15:14 +0100

    Added Terminal component used to render the terminal user interface

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
+import Terminal from './terminal/terminal';
 import TerminalInput from './terminal/terminalInput';
 import TerminalOutputBlock from './terminal/terminalOutputBlock';
 import TerminalErrorBlock from './terminal/terminalErrorBlock';
 
 export {
+  Terminal,
   TerminalInput,
   TerminalOutputBlock,
   TerminalErrorBlock,
---
+++ src/components/terminal/terminal.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { FunctionComponent, ReactElement, ReactNode, useState } from 'react';
+import { BaseProps } from '../../types';
+import { TerminalInput, TerminalErrorBlock } from '..';
+import { useTerminal } from '../../hooks';
+
+/**
+ * The `Terminal` component props
+ */
+interface Props extends BaseProps {
+  readonly children: ReactNode;
+}
+
+/**
+ * Used to render the terminal user interface which
+ * consists of the blocks and command input
+ *
+ * @param props The component props
+ * @returns The `Terminal` component
+ */
+const Terminal: FunctionComponent<Props> = ({ children }): ReactElement<Props> => {
+  const { blocks, execute } = useTerminal();
+  const [inputValue, setInputValue] = useState<string>('');
+
+  return (
+    <div className="h-screen flex flex-col">
+      {children}
+      <div className="h-full flex flex-col-reverse pb-12">
+        {
+          blocks.map((block) => {
+            const { id, type, input } = block;
+
+            // For the error block render the
+            // terminal error block component
+            if (type === 'error') {
+              return (
+                <TerminalErrorBlock
+                  key={`error-block-${id}`}
+                  input={input}
+                  error={block.error}
+                />
+              );
+            }
+
+            // For the output block render the
+            // terminal output block component
+            return (
+              <TerminalOutputBlock
+                key={`output-block-${id}`}
+                input={input}
+              />
+            );
+          })
+        }
+      </div>
+      <TerminalInput
+        value={inputValue}
+        onChange={setInputValue}
+        onEnter={() => execute(inputValue)}
+      />
+    </div>
+  );
+};
+
+export default Terminal;
