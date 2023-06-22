fa778e3e46a6286208861ac9d29b95c8490ebb9c
Liam Hales <liam.hales96@gmail.com>
2023-06-22 22:57:04 +0100

    Updated TerminalExecutedBlock logic to render the feature component and spread the props

--- src/components/terminal/terminalExecutedBlock.tsx
+++ src/components/terminal/terminalExecutedBlock.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, ReactElement } from 'react';
 import { BaseProps, TerminalExecutedBlock as ExecutedBlock } from '../../types';
+import features from '../../features';
 
 /**
  * The `TerminalExecutedBlock` component props
@@ -12,14 +13,27 @@ type Props = Omit<ExecutedBlock, 'id' | 'type'> & BaseProps;
  * @param props The component props
  * @returns The `TerminalExecutedBlock` component
  */
-const TerminalExecutedBlock: FunctionComponent<Props> = ({ input }): ReactElement<Props> => {
+const TerminalExecutedBlock: FunctionComponent<Props> = ({ input, output }): ReactElement<Props> => {
+  const { featureId, props } = output;
 
+  const feature = features.find((feature) => feature.id === featureId);
   const inputText = `> ${input}`;
+
   return (
-    <div className="p-6 border-solid border-t-2 border-zinc-900">
-      <p className="font-mono font-bold text-sm text-white pb-2">
+    <div className="pt-4 pb-4 pl-6 pr-6 border-solid border-t-2 border-zinc-900">
+      <p className="font-mono font-bold text-sm text-zinc-500 pb-3">
         {inputText}
       </p>
+      {
+        // Only render the help feature, all other
+        // features will be rendered differently
+        (
+          feature != null &&
+          feature.id === 'help'
+        ) && (
+          <feature.component {...props} />
+        )
+      }
     </div>
   );
 };
