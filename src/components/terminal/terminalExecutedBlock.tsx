63e0ec2f41c82436aa9922d45221cc1192cc512a
Liam Hales <liam.hales96@gmail.com>
2023-07-25 22:43:09 +0100

    Fixed issues with types in TerminalExecutedBlock component

--- src/components/terminal/terminalExecutedBlock.tsx
+++ src/components/terminal/terminalExecutedBlock.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent, ReactElement } from 'react';
-import { BaseProps, TerminalExecutedBlock as ExecutedBlock } from '../../types';
-import features from '../../features';
+import { BaseProps, TerminalExecutedBlock as ExecutedBlock, FeatureProp } from '../../types';
+import { featureMap } from '../../features';
 
 /**
  * The `TerminalExecutedBlock` component props
@@ -16,7 +16,7 @@ type Props = Omit<ExecutedBlock, 'id' | 'type'> & BaseProps;
 const TerminalExecutedBlock: FunctionComponent<Props> = ({ input, output }): ReactElement<Props> => {
   const { featureId, props } = output;
 
-  const feature = features.find((feature) => feature.id === featureId);
+  const feature = featureMap[featureId];
   const inputText = `> ${input}`;
 
   return (
@@ -24,16 +24,7 @@ const TerminalExecutedBlock: FunctionComponent<Props> = ({ input, output }): Rea
       <p className="font-mono font-bold text-sm text-zinc-500 pb-3">
         {inputText}
       </p>
-      {
-        // Only render the help feature, all other
-        // features will be rendered differently
-        (
-          feature != null &&
-          feature.id === 'help'
-        ) && (
-          <feature.component {...props} />
-        )
-      }
+      <feature.component {...props as FeatureProp} />
     </div>
   );
 };
