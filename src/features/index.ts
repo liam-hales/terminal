993a052299a9072950d163f65d72437f36560cf0
Liam Hales <liam.hales96@gmail.com>
2023-07-25 22:36:08 +0100

    Added feature map and FeatureMap type

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,7 +1,26 @@
+import { FeatureMap } from './types';
 import helpFeature from './help.feature';
+import base64Feature from './base64.feature';
+
+/**
+ * Describes all the features
+ * as an array
+ */
+export const features = [
+  helpFeature,
+  base64Feature,
+];
+
+/**
+ * Describes all the features as a map between the
+ * feature `id` and the corresponding feature
+ */
+export const featureMap = features.reduce<FeatureMap>((map, feature) => {
+  const { id } = feature;
+  return {
+    ...map,
+    [id]: feature,
+  };
+}, {} as FeatureMap);
 
 export * from './types';
-
-export default [
-  helpFeature,
-];
--- src/features/types.ts
+++ src/features/types.ts
@@ -1,7 +1,20 @@
-import features from '.';
+import { features } from '.';
 
 /**
  * The feature type used to
  * describe all features
  */
 export type Feature = typeof features[number];
+
+/**
+ * The feature map between the feature `id`
+ * and the corresponding feature
+ */
+export type FeatureMap = {
+  [K in Feature['id']]: Extract<
+    Feature,
+    {
+      readonly id: K;
+    }
+  >;
+};
