3b52bc5ef12cf5d2f7cd0b2deef8c99734ad06df
Liam Hales <liam.hales96@gmail.com>
2023-09-05 23:22:43 +0100

    Updated styles and added use of CodeInline component

--- src/components/help/help.tsx
+++ src/components/help/help.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent, ReactElement } from 'react';
 import { features } from '../../features';
+import { CodeInline } from '../common';
 
 /**
  * Used to render the help user interface
@@ -13,32 +14,43 @@ const Help: FunctionComponent = (): ReactElement => {
     <>
       <p className="font-mono text-sm text-white pb-8">
         To get help for a specific command, use
-        {' "help --for <command>"'}
+        <CodeInline>
+          {'help --for <command>'}
+        </CodeInline>
       </p>
       <p className="font-mono text-sm text-white">
         Commands:
       </p>
-      {
-        features
-          .map((feature) => feature.command)
-          .map((command) => {
-            const { name, description } = command;
+      <div className="flex flex-col gap-y-1 pt-2 pl-4">
+        {
+          features
+            .map((feature) => feature.command)
+            .map((command) => {
+              const { name, description } = command;
 
-            return (
-              <div
-                className="flex flex-row pl-4"
-                key={`help-command-${name}`}
-              >
-                <p className="w-28 shrink-0 font-mono text-sm text-white">
-                  {name}
-                </p>
-                <p className="font-mono text-sm text-white">
-                  {description}
-                </p>
-              </div>
-            );
-          })
-      }
+              return (
+                <div
+                  className="flex flex-row"
+                  key={`help-command-${name}`}
+                >
+                  <div className="w-24 shrink-0">
+                    <CodeInline>
+                      {name}
+                    </CodeInline>
+                  </div>
+                  <div className="flex flex-row mt-[2px]">
+                    <p className="font-mono text-sm font-bold text-white pl-2 pr-2">
+                      -
+                    </p>
+                    <p className="font-mono text-sm text-white">
+                      {description}
+                    </p>
+                  </div>
+                </div>
+              );
+            })
+        }
+      </div>
     </>
   );
 };
