44d1763b0ebcb5abdbde8e9911bc4e95758404b3
Liam Hales <liam.hales96@gmail.com>
2025-01-30 22:26:48 +0000

    Added missing postcss config file for tailwindcss v4

---
+++ postcss.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * The PostCSS config used to
+ * configure PostCSS for the app
+ */
+const postcssConfig = {
+  plugins: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '@tailwindcss/postcss': {},
+  },
+};
+
+export default postcssConfig;
