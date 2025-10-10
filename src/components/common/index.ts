9563208112eef50269a1bf18663900a4fdead20d
Liam Hales <liam.hales96@gmail.com>
2025-10-10 23:40:14 +0100

    Removed unused `Typewriter` component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,11 +1,9 @@
 import CodeInline from './codeInline';
 import Loader from './loader';
-import Typewriter from './typewriter';
 import ProgressBar from './progressBar';
 
 export {
   CodeInline,
   Loader,
-  Typewriter,
   ProgressBar,
 };
