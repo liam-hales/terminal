adfbc13c462afca4397b63ec75db5670c36a58f8
Liam Hales <liam.hales96@gmail.com>
2026-01-24 23:42:35 +0000

    Updated `SharePage` component styles and added "Shared from" section

--- src/app/share/[id]/page.tsx
+++ src/app/share/[id]/page.tsx
@@ -5,8 +5,9 @@ import { fetchShareItem } from '../../../database';
 import { useParams } from 'next/navigation';
 import { decryptData } from '../../../helpers';
 import { Loader } from '../../../components/common';
-import { TerminalBlock } from '../../../components';
+import { TerminalTitle, TerminalBlock } from '../../../components';
 import { TerminalBlock as Block } from '../../../types';
+import Link from 'next/link';
 
 /**
  * The share page component, used to fetch and
@@ -57,28 +58,44 @@ const SharePage: FunctionComponent = (): ReactElement => {
   }, [id]);
 
   return (
-    <div className="w-full flex flex-col items-start gap-y-10 pt-6 pl-6 pr-6 overflow-y-auto no-scrollbar touch-pan-y">
-      {
-        (blocks == null)
-          ? (
-              <div className="flex flex-row items-center gap-x-4">
-                <Loader />
-                <p className="text-xs">
-                  Loading...
-                </p>
-              </div>
-            )
-          : blocks.map((block) => {
-              const { id, type } = block;
+    <div className="w-full h-full flex flex-col items-center pt-6 pb-10 pl-6 pr-6">
+      <div className="w-full max-w-200 h-full flex flex-col items-start gap-y-8">
+        <div className="w-full flex flex-col items-start border-solid border border-primary/20 rounded-sm gap-y-10 p-6 overflow-y-auto no-scrollbar touch-pan-y">
+          {
+            (blocks == null)
+              ? (
+                  <div className="flex flex-row items-center gap-x-4">
+                    <Loader />
+                    <p className="text-xs">
+                      Loading...
+                    </p>
+                  </div>
+                )
+              : blocks.map((block) => {
+                  const { id, type } = block;
 
-              return (
-                <TerminalBlock
-                  key={`terminal-${type}-block-${id}`}
-                  {...block}
-                />
-              );
-            })
-      }
+                  return (
+                    <TerminalBlock
+                      key={`terminal-${type}-block-${id}`}
+                      {...block}
+                    />
+                  );
+                })
+          }
+        </div>
+        <div className="w-full flex flex-col items-start gap-y-2 pl-4">
+          <p className="text-sm">
+            Shared from
+          </p>
+          <Link
+            href={`https://${window.location.hostname}`}
+            target="_blank"
+            passHref={true}
+          >
+            <TerminalTitle className="text-[5px] leading-[6.4px]" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
