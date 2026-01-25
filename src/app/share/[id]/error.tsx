e6caaa1f56177d84a293e15a4a57a791a69921e6
Liam Hales <liam.hales96@gmail.com>
2026-01-25 15:25:37 +0000

    Added `ShareError` error boundary used to catch any errors thrown while fetching and decrypting share items

---
+++ src/app/share/[id]/error.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ReactElement, useEffect } from 'react';
+import { ErrorComponent } from 'next/dist/client/components/error-boundary';
+
+/**
+ * The share error boundary used to catch any errors
+ * thrown while fetching and decrypting share items
+ *
+ * @returns The `ShareError` component
+ */
+const ShareError: ErrorComponent = ({ error }): ReactElement => {
+
+  /**
+   * Logs out the error to the console
+   * for extra visibility
+   */
+  // eslint-disable-next-line no-console
+  useEffect(() => console.error(error), [error]);
+
+  return (
+    <div className="w-full max-w-200 h-full flex flex-col items-center pt-6 pb-10 pl-6 pr-6">
+      <div className="w-full flex flex-col items-start border border-l-10 border-error pt-6 pb-6 pl-4 pr-20 gap-y-2">
+        <p className="text-xs text-error!">
+          {`X - ${error.message}`}
+        </p>
+        <p className="text-xs italic text-error! pl-8">
+          Please check the URL is correct
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ShareError;
