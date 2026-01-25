f413e229705b20ba797a2e63b597305d8395312c
Liam Hales <liam.hales96@gmail.com>
2026-01-25 11:39:01 +0000

    Merged `div` elements and added check to only show "Shared from" section when data has been decrypted

--- src/app/share/[id]/page.tsx
+++ src/app/share/[id]/page.tsx
@@ -58,44 +58,46 @@ const SharePage: FunctionComponent = (): ReactElement => {
   }, [id]);
 
   return (
-    <div className="w-full h-full flex flex-col items-center pt-6 pb-10 pl-6 pr-6">
-      <div className="w-full max-w-200 h-full flex flex-col items-start justify-between gap-y-8">
-        <div className="w-full flex flex-col items-start border-solid border border-primary/20 rounded-sm gap-y-10 p-6 overflow-y-auto no-scrollbar touch-pan-y">
-          {
-            (blocks == null)
-              ? (
-                  <div className="flex flex-row items-center gap-x-4">
-                    <Loader />
-                    <p className="text-xs">
-                      Loading...
-                    </p>
-                  </div>
-                )
-              : blocks.map((block) => {
-                  const { id, type } = block;
+    <div className="w-full max-w-200 h-full flex flex-col items-start justify-between gap-y-8 pt-6 pb-10 pl-6 pr-6">
+      <div className="w-full flex flex-col items-start border-solid border border-primary/20 rounded-sm gap-y-10 p-6 overflow-y-auto no-scrollbar touch-pan-y">
+        {
+          (blocks == null)
+            ? (
+                <div className="flex flex-row items-center gap-x-4">
+                  <Loader />
+                  <p className="text-xs">
+                    Loading...
+                  </p>
+                </div>
+              )
+            : blocks.map((block) => {
+                const { id, type } = block;
 
-                  return (
-                    <TerminalBlock
-                      key={`terminal-${type}-block-${id}`}
-                      {...block}
-                    />
-                  );
-                })
-          }
-        </div>
-        <div className="w-full flex flex-col items-start gap-y-2 pl-4">
-          <p className="text-sm">
-            Shared from
-          </p>
-          <Link
-            href={`https://${window.location.hostname}`}
-            target="_blank"
-            passHref={true}
-          >
-            <TerminalTitle className="text-[5px] leading-[6.4px]" />
-          </Link>
-        </div>
+                return (
+                  <TerminalBlock
+                    key={`terminal-${type}-block-${id}`}
+                    {...block}
+                  />
+                );
+              })
+        }
       </div>
+      {
+        (blocks != null) && (
+          <div className="w-full flex flex-col items-start gap-y-2 pl-4">
+            <p className="text-sm">
+              Shared from
+            </p>
+            <Link
+              href={`https://${window.location.hostname}`}
+              target="_blank"
+              passHref={true}
+            >
+              <TerminalTitle className="text-[5px] leading-[6.4px]" />
+            </Link>
+          </div>
+        )
+      }
     </div>
   );
 };
