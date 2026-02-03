e337bcf941e1e4608e4ce8ba72dc4b6484d1a9b8
Liam Hales <liam.hales96@gmail.com>
2026-02-03 20:42:28 +0000

    Updated `ShareError` component styles

--- src/app/share/[id]/error.tsx
+++ src/app/share/[id]/error.tsx
@@ -20,7 +20,7 @@ const ShareError: ErrorComponent = ({ error }): ReactElement => {
 
   return (
     <div className="w-full max-w-200 h-full flex flex-col items-center pt-6 pb-10 pl-6 pr-6">
-      <div className="w-full flex flex-col items-start border border-l-10 border-error pt-6 pb-6 pl-4 pr-20 gap-y-2">
+      <div className="w-full flex flex-col items-start border-solid border border-error shadow-error/60 shadow-[5px_5px_0px_0px] rounded-sm pt-6 pb-6 pl-4 pr-20 gap-y-2">
         <p className="text-xs text-error!">
           {`X - ${error.message}`}
         </p>
