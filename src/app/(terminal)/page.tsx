988bc979ca97bb613971e84e86b7a4755785cc9c
Liam Hales <liam.hales96@gmail.com>
2026-02-04 23:36:20 +0000

    Updated `TerminalPage` links and added copyright notice with dynamic year

--- src/app/(terminal)/page.tsx
+++ src/app/(terminal)/page.tsx
@@ -4,6 +4,7 @@ import { FunctionComponent, ReactElement } from 'react';
 import { CodeInline } from '../../components/common';
 import { TerminalTitle } from '../../components';
 import Link from 'next/link';
+import date from '../../date';
 
 /**
  * The terminal page component used to render the welcome UI
@@ -12,25 +13,43 @@ import Link from 'next/link';
  * @returns The `TerminalPage` component
  */
 const TerminalPage: FunctionComponent = (): ReactElement => {
+
+  const year = date
+    .utc()
+    .format('YYYY');
+
   return (
     <div className="w-full flex flex-col items-start pt-12 pb-12">
-      <TerminalTitle className="leading-5" />
-      <div className="flex flex-col pt-16">
+      <div className="flex flex-col items-end gap-y-2">
+        <TerminalTitle className="leading-5" />
+        <p className="text-[11px]">
+          {`© ${year} <LIAM HALES>`}
+        </p>
+      </div>
+      <div className="flex flex-col pt-12 gap-y-8">
         <p className="max-w-110 text-xs">
           Web-based developer tools with a dev-friendly terminal interface.
         </p>
-        <p className="text-xs pt-8">
-          <span className="italic mr-2">- Built by </span>
+        <div className="flex flex-row items-center gap-x-6">
           <Link
             href="https://liamhales.dev"
             target="_blank"
             passHref={true}
           >
-            <CodeInline>
-              LIAM HALES
-            </CodeInline>
+            <p className="text-xs border-solid border border-primary shadow-primary/60 shadow-[4px_4px_0px_0px] p-2">
+              liamhales.dev
+            </p>
           </Link>
-        </p>
+          <Link
+            href="https://github.com/liam-hales/terminal"
+            target="_blank"
+            passHref={true}
+          >
+            <p className="text-xs border-solid border border-primary shadow-primary/60 shadow-[4px_4px_0px_0px] p-2">
+              source / github
+            </p>
+          </Link>
+        </div>
       </div>
       <div className="flex flex-col pt-24">
         <p className="text-xl">
